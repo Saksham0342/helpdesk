b8739bee59c53e177096d36b1a85837bdf8d5211
Saksham0342 <saksham.23rastogi@gmail.com>
2025-07-03 20:06:12 +0530

    add environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,19 @@
-// Replace with your actual config from Firebase console
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBsaG9I_igY9MaB7C5LlWqIFlrMl8WzS2U",
-  authDomain: "helpdesk-6ca3f.firebaseapp.com",
-  projectId: "helpdesk-6ca3f",
-  storageBucket: "helpdesk-6ca3f.firebasestorage.app",
-  messagingSenderId: "1027932912511",
-  appId: "1:1027932912511:web:ef27b9e771430fc6e5384a",
-  measurementId: "G-7Q5JFZ1B28"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export { auth, db };
